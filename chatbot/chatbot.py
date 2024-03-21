355b63f3d7f7feb507a6310dcdf528c40851f79f
igregorioneto <igregoriodoneto@outlook.com>
2024-03-21 10:55:38 -0300

    feat: modify chatbot

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -8,19 +8,20 @@ PORT = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-
+    print(f"Servidor executado em : {HOST}:{PORT}")
     # Aceita conexão com o cliente
     conn, addr = s.accept()
-    with conn:
-        print(f"Conectado por: {addr}")
-        while True:
-            # Recebe mensagem do Cliente
-            data = conn.recv(1024)
-            if not data:
-                break
-            message = data.decode()
+    print(f"Conectado por: {addr}")
+    while True:
+        with conn:            
+            while True:
+                # Recebe mensagem do Cliente
+                data = conn.recv(1024)
+                if not data:
+                    break
+                message = data.decode()
 
-            response = f"Response: {message}"
+                response = f"{message}"
 
-            # Envia mensagem para o cliente
-            conn.sendall(response.encode())
+                # Envia mensagem para o cliente
+                conn.sendall(response.encode())
--- chatbot/cli.js
+++ chatbot/cli.js
@@ -8,5 +8,35 @@ const PORT = 65432
 const client = new net.Socket()
 client.connect(PORT, HOST, () => {
     console.log('Conectado ao servidor')
-    
+
+    function sendMessage() {
+        // Interação com o usuário
+        const readline = require('readline')
+        const r1 = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
+
+        // Mensagem para o servidor
+        r1.question('Você: ', (input) => {
+            client.write(input)
+        })
+
+        // Receber mensagem do servidor
+        client.on('data', (data) => {
+            message = data.toString()
+            if (message === 'sair') {
+                client.destroy()
+            }
+            console.log('ChatBot: ', message)
+            sendMessage()
+        })
+    }
+
+    sendMessage()
 })
+
+// Tratar eventos de erro
+client.on('error', (err) => [
+    console.error('Erro: ', err.message)
+])
