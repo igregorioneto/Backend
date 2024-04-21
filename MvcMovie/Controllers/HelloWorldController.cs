c8e2421044dc791aed9f19adecb2931c221791e7
igregorioneto <igregoriodoneto@outlook.com>
2024-04-21 09:15:45 -0300

    feat: projeto inicial MVC e Adicionando um Controlador

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
